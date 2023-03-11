538ce82d0294c796666c1535c672baf4554699c8
Garm <melsom.henrik@gmail.com>
2023-03-11 20:10:35 +0100

    ⚡ Add mark up ui derives

--- bevy_markup_ui/src/widgets.rs
+++ bevy_markup_ui/src/widgets.rs
@@ -4,57 +4,16 @@ use crate::{
     widget::Widget,
     widget_registry::{GetWidgetRegistration, WidgetRegistration},
 };
+use bevy_markup_ui_derive::Widget;
 
+#[derive(Widget)]
+#[bundle]
 pub struct Node;
 
-impl GetWidgetRegistration for Node {
-    fn get_widget() -> Box<dyn Widget> {
-        Box::new(Self)
-    }
-
-    fn get_widget_registration() -> WidgetRegistration {
-        WidgetRegistration::of::<Self>()
-    }
-}
-
-impl Widget for Node {
-    fn spawn<'w>(&self, parent: &'w mut WorldChildBuilder) -> EntityMut<'w> {
-        parent.spawn(NodeBundle::default())
-    }
-}
-
+#[derive(Widget)]
+#[bundle]
 pub struct Button;
 
-impl GetWidgetRegistration for Button {
-    fn get_widget() -> Box<dyn Widget> {
-        Box::new(Self)
-    }
-
-    fn get_widget_registration() -> WidgetRegistration {
-        WidgetRegistration::of::<Self>()
-    }
-}
-
-impl Widget for Button {
-    fn spawn<'w>(&self, parent: &'w mut WorldChildBuilder) -> EntityMut<'w> {
-        parent.spawn(ButtonBundle::default())
-    }
-}
-
+#[derive(Widget)]
+#[bundle]
 pub struct Text;
-
-impl GetWidgetRegistration for Text {
-    fn get_widget() -> Box<dyn Widget> {
-        Box::new(Self)
-    }
-
-    fn get_widget_registration() -> WidgetRegistration {
-        WidgetRegistration::of::<Self>()
-    }
-}
-
-impl Widget for Text {
-    fn spawn<'w>(&self, parent: &'w mut WorldChildBuilder) -> EntityMut<'w> {
-        parent.spawn(TextBundle::default())
-    }
-}
---
+++ bevy_markup_ui_derive/src/lib.rs
@@ -0,0 +1,47 @@
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Widget, attributes(bundle))]
+pub fn derive_widget(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let DeriveInput { ident, attrs, .. } = parse_macro_input!(item as DeriveInput);
+
+    let get_widget_registration_impl = get_widget_registration(&ident);
+
+    let bundle_widget_impl = attrs
+        .iter()
+        .find(|&attr| attr.path.is_ident(&Ident::new("bundle", Span::call_site())))
+        .map(|_| bundle_widget(&ident));
+
+    quote! {
+        #get_widget_registration_impl
+
+        #bundle_widget_impl
+    }
+    .into()
+}
+
+fn get_widget_registration(ident: &Ident) -> TokenStream {
+    quote! {
+        impl GetWidgetRegistration for #ident {
+            fn get_widget() -> Box<dyn Widget> {
+                Box::new(Self)
+            }
+
+            fn get_widget_registration() -> WidgetRegistration {
+                WidgetRegistration::of::<Self>()
+            }
+        }
+    }
+}
+
+fn bundle_widget(ident: &Ident) -> TokenStream {
+    let bundle = Ident::new(&format!("{}Bundle", ident), Span::call_site());
+    quote! {
+        impl Widget for #ident {
+            fn spawn<'w>(&self, parent: &'w mut WorldChildBuilder) -> EntityMut<'w> {
+                parent.spawn(#bundle::default())
+            }
+        }
+    }
+}
